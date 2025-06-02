73e2bcddbe80e29ba4093aed43010616f41ff388
김승혁 <peter9889466@gmail.com>
2025-06-02 19:32:47 +0900

    Animation Dice

--- team_dice/dice-app-ksh/src/App.jsx
+++ team_dice/dice-app-ksh/src/App.jsx
@@ -3,10 +3,20 @@ import './App.css'
 // import { DiceGame } from './components/DiceGame'
 import { DiceGame2 } from './components2/DiceGame2'
 
+// function App() {
+//   return (
+//     <DiceGame />
+//   )
+// }
+
 function App() {
   return (
-    <DiceGame2 />
-  )
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>🎲 주사위 게임</h1>
+      <DiceGame2 />
+    </div>
+  );
 }
+
 
 export default App
--- team_dice/dice-app-ksh/src/components2/DiceGame2.jsx
+++ team_dice/dice-app-ksh/src/components2/DiceGame2.jsx
@@ -1,60 +1,18 @@
-import React, { useState } from 'react';
-import ButtonArea2 from './ButtonArea2';
-import BoardArea2 from './BoardArea2';
-
+import React, { useEffect, useRef ,useState} from 'react';
+import DiceDisplay from './DiceDisplay';
+import RollButton from './RollButton';
 export const DiceGame2 = () => {
-    const [playerNum, setPlayerNum] = useState(1);
-    const [comNum, setComNum] = useState(1);
+    const [diceNumber, setDiceNumber] = useState(1);
 
-    const [winCount, setWinCount] = useState(0);
-    const [loseCount, setLoseCount] = useState(0);
-    const [drawCount, setDrawCount] = useState(0);
-
-    const [isRolling, setIsRolling] = useState(false);
-
-    const rollDice = () => {
-        setIsRolling(true); // 1초 동안 rolling 이미지 표시
-
-        setTimeout(() => {
-            const newPlayerNum = Math.floor(Math.random() * 6) + 1;
-            const newComNum = Math.floor(Math.random() * 6) + 1;
-
-            setPlayerNum(newPlayerNum);
-            setComNum(newComNum);
-
-            if (newPlayerNum > newComNum) {
-                setWinCount(prev => prev + 1);
-            } else if (newPlayerNum < newComNum) {
-                setLoseCount(prev => prev + 1);
-            } else {
-                setDrawCount(prev => prev + 1);
-            }
-            setIsRolling(false);
-        }, 1000);
-    };
-
-    const reset = () => {
-        setPlayerNum(1);
-        setComNum(1);
-        setWinCount(0);
-        setLoseCount(0);
-        setDrawCount(0);
-        setResult('');
-        setIsRolling(false);
+    const handleRoll = () => {
+        const randomNum = Math.floor(Math.random() * 6 + 1);
+        setDiceNumber(randomNum);
     };
 
     return (
-        <div className='diceGame2'>
-            <h1 style={{marginTop:'1px'}} >주사위 게임</h1>
-            <ButtonArea2 rollDice={rollDice} reset={reset} />
-            <BoardArea2
-                playerNum={playerNum}
-                comNum={comNum}
-                isRolling={isRolling}  //상태값으로 전달
-            />
-            <div style={{ textAlign: 'center', marginTop: '10px' }}>
-                <strong><p>승: {winCount} | 패: {loseCount} | 무: {drawCount}</p></strong>
-            </div>
+        <div>
+            <DiceDisplay diceNumber={diceNumber} />
+            <RollButton onRoll={handleRoll} />
         </div>
     );
 };
