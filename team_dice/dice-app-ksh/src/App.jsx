371fc9cc86e3e4f10bb93396318489940521ec3f
김승혁 <peter9889466@gmail.com>
2025-06-10 14:58:26 +0900

    committ

--- team_dice/dice-app-ksh/src/App.jsx
+++ team_dice/dice-app-ksh/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-// import { DiceGame } from './components/DiceGame'
-import { DiceGame2 } from './components2/DiceGame2'
-
-// function App() {
-//   return (
-//     <DiceGame />
-//   )
-// }
+import { DiceGame } from './components/DiceGame'
+// import { DiceGame2 } from './components2/DiceGame2'
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ textAlign: 'center' }}>🎲 주사위 게임</h1>
-      <DiceGame2 />
-    </div>
-  );
+    <DiceGame />
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1 style={{ textAlign: 'center' }}>🎲 주사위 게임</h1>
+//       <DiceGame2 />
+//     </div>
+//   );
+// }
 
 
 export default App
